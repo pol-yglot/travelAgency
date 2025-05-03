3a47b6569366e572c710897658faf260e569a5bd
hoyoungparkme <hoyoungpark.ds@gmail.com>
2025-05-03 17:43:53 +0900

    docs: 커밋테스트
    커밋테스트

--- travelAgency/api_module/train_model.py
+++ travelAgency/api_module/train_model.py
@@ -2,8 +2,8 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 import joblib
 
-X = np.array([[1], [2], [3], [4]])
-y = np.array([3, 5, 7, 9])  # y = 2x + 1
+X = np.array([[1], [2], [3], [5]])
+y = np.array([3, 5, 7, 11])  # y = 2x + 1
 
 model = LinearRegression()
 model.fit(X, y)
